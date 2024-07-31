81114a162bd080563a7a8e94267fdeb00511a860
bukhaled2002 <Bakhaled.310@gmail.com>
2024-07-31 17:03:05 +0300

    finishing the project

--- client/src/utils/customFetch.js
+++ client/src/utils/customFetch.js
@@ -2,12 +2,11 @@ import axios from "axios";
 import Cookies from "js-cookie";
 
 const customFetch = axios.create({
-  baseURL: "https://e-commerce-vuni.onrender.com/api/v1",
+  baseURL: "http://localhost:3000/api/v1",
   headers: { "Content-Type": "application/json" },
 });
 customFetch.interceptors.request.use((config) => {
   const authToken = Cookies.get("jwt");
-  console.log(authToken);
   if (authToken) {
     config.headers.Authorization = `Bearer ${authToken}`;
   }
