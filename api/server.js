c2c94ff046155fc317e2819f37b686f72d5399f0
bukhaled2002 <Bakhaled.310@gmail.com>
2024-08-02 09:17:59 +0300

    finishing the project

--- api/server.js
+++ api/server.js
@@ -32,7 +32,7 @@ app.use(
       "http://localhost:5173",
     ],
     allowedHeaders: ["Content-Type", "Authorization"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
 );
